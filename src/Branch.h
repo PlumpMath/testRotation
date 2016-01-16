ba883a8f06a6bd7e01c4088ecf961e731ed6453b
edapx <lastexxit@gmail.com>
2016-01-16 19:12:03 +0100

    use ofNode, add joint branch

--- src/Branch.h
+++ src/Branch.h
@@ -2,19 +2,5 @@
 #include "ofMain.h"
 
 class Branch: public ofNode{
-    ofNode translation;
-    
-public:
-    Branch(){
-        ofNode::setParent(translation); // this calls the ofNode version of setParent
-        // avoiding a recursive call when calling our own version
-    }
-    
-    void preTranslate(ofVec3f t){
-        translation.move(t);
-    }
-    
-    void setParent(ofNode & node){
-        translation.setParent(node);
-    }
+
 };
